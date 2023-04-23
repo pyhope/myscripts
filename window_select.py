dce556e35e34c2ece364925dba173e28d4c6dcb2
Yihang Peng <ypeng0082@gmail.com>
2023-04-23 01:35:37 -0400

    window

---
+++ window_select.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import matplotlib.pyplot as plt
+import pickle as pkl
+import argparse
+from matplotlib import rcParams
+
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.size'] = '16'
+rcParams['font.sans-serif'] = 'Arial'
+rcParams['mathtext.fontset'] = 'custom'
+rcParams['mathtext.rm'] = 'Arial'
+rcParams['mathtext.it'] = 'Arial:italic'
+rcParams['mathtext.bf'] = 'Arial:bold'
+rcParams['xtick.direction'] = 'in'
+rcParams['ytick.direction'] = 'in'
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input_file", type=str,  help="Input file")
+args = parser.parse_args()
+
+with open(args.input_file + '.pkl', 'rb') as file:
+    tcorr, j_t500, j_t1ns, j_t2ns, k_t500, k_t1ns, k_t2ns = pkl.load(file)
+tcorr_idx = range(len(tcorr))
+
+fig,ax = plt.subplots(2,1,figsize=(8,12),sharex=True)
+
+ax[0].plot(tcorr, j_t2ns[tcorr_idx])
+ax[1].plot(tcorr, k_t2ns[tcorr_idx])
+ax[1].set_xlabel('t (ps)')
+ax[1].set_ylabel(r'$k $'+' '+ r'$(\mathrm{W} \mathrm{m}^{-1} \mathrm{K}^{-1})$')
+
+plt.gca().set_xlim(2e-4, 50)
+
+ax[1].set_xscale('log')
+ax[1].set_ylim(0, 6)
+plt.show()
