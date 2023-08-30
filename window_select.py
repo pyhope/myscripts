2de49d670d8a08e2c2e6e7dbcc7aa624a6a93dc8
Yihang Peng <ypeng0082@gmail.com>
2023-08-30 13:25:56 -0400

    update

--- window_select.py
+++ window_select.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import numpy as np
 import matplotlib.pyplot as plt
 import pickle as pkl
 import argparse
@@ -20,18 +21,17 @@ parser.add_argument("input_file", type=str,  help="Input file")
 args = parser.parse_args()
 
 with open(args.input_file, 'rb') as file:
-    tcorr, tl, j_list, k_list = pkl.load(file)
+    T, P, tcorr, jxyz, kxyz = pkl.load(file)
 tcorr_idx = range(len(tcorr))
 
 fig,ax = plt.subplots(2,1,figsize=(8,12),sharex=True)
 
-ax[0].plot(tcorr, j_list[-1][tcorr_idx])
-ax[1].plot(tcorr, k_list[-1][tcorr_idx])
+ax[0].plot(tcorr, np.mean(jxyz, axis=0))
+ax[1].plot(tcorr, np.mean(kxyz, axis=0))
 ax[1].set_xlabel('t (ps)')
 ax[1].set_ylabel(r'$k $'+' '+ r'$(\mathrm{W} \mathrm{m}^{-1} \mathrm{K}^{-1})$')
 
-plt.gca().set_xlim(2e-4, 50)
+plt.gca().set_xlim(2e-4, 100)
 
 ax[1].set_xscale('log')
-ax[1].set_ylim(0, 10)
 plt.show()
