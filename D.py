5572a39167695be959d1bdc99191a9efd5273223
Yihang Peng <ypeng0082@gmail.com>
2023-09-05 00:29:44 -0400

    update

--- D.py
+++ D.py
@@ -7,14 +7,14 @@ import glob
 #from matplotlib import pyplot as plt
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--file","-f",type=str, default = 'msd_fft.txt' ,help="input file")
+parser.add_argument("--file","-i",type=str, default = 'msd_fft.txt' ,help="input file")
 parser.add_argument("--lim1","-l1",type = float, default = 1e5, help="time threshold 1")
 parser.add_argument("--lim2","-l2",type = float, default = 5e5, help="time threshold 2")
 parser.add_argument("--timestep","-ts",type = int, default = 50, help="time steps")
-parser.add_argument("--output","-of",type = str, default = 'D_H', help="output filename")
+parser.add_argument("--output","-o",type = str, default = 'D_H', help="output filename")
 
 args   = parser.parse_args()
-
+timestep =args.timestep
 
 def fit(x, y, deg):
     z = np.polyfit(x, y, deg=deg, full=True)
@@ -22,28 +22,37 @@ def fit(x, y, deg):
     R_square = 1 - z[1][0] / sum((y - np.mean(y))** 2)
     return pn, R_square
 
+def CalcMSD(msd):
+    t = np.array(list(range(len(msd)))) * timestep
+
+    for i in range(len(t)):
+        if t[i] >= args.lim1:
+            index1 = i
+            break
+    for i in range(len(t)):
+        if t[i] >= args.lim2:
+            index2 = i
+            break
+    index2 = i
+    pn, Rs = fit(t[index1:index2], msd[index1:index2], 1)
+
+    D = pn[1]/6 # unit: A^2/fs
+    D = 1000* D # unit: A^2/ps
+    D = 10 * D # unit: * 1e-9 m^2/s
+
+    print(t[index1:index2])
+    print("D_H = ", D, "* 1e-9 m^2/s")
+    print("R^2 = ", Rs)
+    return D
+
 msd = np.loadtxt(args.file, skiprows=1)
-timestep =args.timestep
-t = np.array(list(range(len(msd)))) * timestep
-
-for i in range(len(t)):
-    if t[i] >= args.lim1:
-        index1 = i
-        break
-for i in range(len(t)):
-    if t[i] >= args.lim2:
-        index2 = i
-        break
-index2 = i
-pn, Rs = fit(t[index1:index2], msd[index1:index2], 1)
-
-D = pn[1]/6 # unit: A^2/fs
-D = 1000* D # unit: A^2/ps
-D = 10 * D # unit: * 1e-9 m^2/s
-
-print(t[index1:index2])
-print("D_H = ", D, "* 1e-9 m^2/s")
-print("R^2 = ", Rs)
-
-dataout = open(args.output, "w")
-dataout.write(str(D)+' ')
+if msd.ndim == 1:
+    D = CalcMSD(msd)
+    dataout = open(args.output, "w")
+    dataout.write(str(D)+' ')
+elif msd.ndim == 2:
+    dataout = open(args.output, "w")
+    for index, row in enumerate(msd.T):
+        print('Element:', index + 1)
+        D = CalcMSD(row)
+        dataout.write(str(D)+'\n')
