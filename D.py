0675a3ca671a34fa9b8e35167cdc9ca3778a064f
pyhope <ypeng0082@gmail.com>
2024-01-05 01:47:03 -0500

    update

--- D.py
+++ D.py
@@ -1,65 +1,69 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# author: Yihang Peng
+
 import numpy as np
 import argparse
 from matplotlib import pyplot as plt
+from scipy.stats import linregress
 
+# Argument parser for command line options
 parser = argparse.ArgumentParser()
-parser.add_argument("--file","-i",type=str, default = 'msd_fft.txt' ,help="input file")
-parser.add_argument("--lim1","-l1",type = float, default = 1e5, help="time threshold 1")
-parser.add_argument("--lim2","-l2",type = float, default = 5e5, help="time threshold 2")
-parser.add_argument("--timestep","-ts",type = float, default = 50, help="time steps")
-parser.add_argument("--output","-o",type = str, default = 'D_H', help="output filename")
+parser.add_argument("--file", "-i", type=str, default='msd_fft.txt', help="input file")
+parser.add_argument("--lim1", "-l1", type=float, default=1e5, help="time threshold 1")
+parser.add_argument("--lim2", "-l2", type=float, default=5e5, help="time threshold 2")
+parser.add_argument("--timestep", "-ts", type=float, default=50, help="time steps")
+parser.add_argument("--output", "-o", type=str, default='D_H', help="output filename")
+parser.add_argument("--error_output", "-eo", type=str, default='D_err', help="error output filename")
 
-args   = parser.parse_args()
-timestep =args.timestep
+args = parser.parse_args()
+timestep = args.timestep
 
-def fit(x, y, deg):
-    z = np.polyfit(x, y, deg=deg, full=True)
-    pn = np.poly1d(z[0])
-    R_square = 1 - z[1][0] / sum((y - np.mean(y))** 2)
-    return pn, R_square
+# Function for linear fitting
+def fit(x, y):
+    slope, intercept, r_value, p_value, std_err = linregress(x, y)
+    return slope, intercept, r_value**2, std_err
 
+# Function to calculate MSD
 def CalcMSD(msd):
     t = np.array(list(range(len(msd)))) * timestep
+    index1 = np.argmax(t >= args.lim1)
+    index2 = np.argmax(t >= args.lim2)
+    
+    slope, intercept, r_squared, std_err = fit(t[index1:index2], msd[index1:index2])
 
-    for i in range(len(t)):
-        if t[i] >= args.lim1:
-            index1 = i
-            break
-    for i in range(len(t)):
-        if t[i] >= args.lim2:
-            index2 = i
-            break
-    index2 = i
-    pn, Rs = fit(t[index1:index2], msd[index1:index2], 1)
+    D = slope / 6  # unit: A^2/fs
+    D = 1000 * D  # unit: A^2/ps
+    D = 10 * D   # unit: * 1e-9 m^2/s
+    D_error = std_err / 6 * 10000  # unit: * 1e-9 m^2/s
 
-    D = pn[1]/6 # unit: A^2/fs
-    D = 1000* D # unit: A^2/ps
-    D = 10 * D # unit: * 1e-9 m^2/s
+    print(f"D = {D} ± {D_error} * 1e-9 m^2/s")
+    print(f"R^2 = {r_squared}")
+    return D, D_error
 
-    print(t[index1:index2])
-    print("D = ", D, "* 1e-9 m^2/s")
-    print("R^2 = ", Rs)
-    return D
-
+# Plotting and analysis
 fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(111)
 msd = np.loadtxt(args.file, skiprows=1)
+
+dataout = open(args.output, "w")
+errorout = open(args.error_output, "w")
+
 if msd.ndim == 1:
     ax.plot(np.array(list(range(len(msd)))) * timestep, msd)
-    D = CalcMSD(msd)
-    dataout = open(args.output, "w")
-    dataout.write(str(D)+' ')
+    D, D_error = CalcMSD(msd)
+    dataout.write(str(D) + ' ')
+    errorout.write(str(D_error) + ' ')
 elif msd.ndim == 2:
-    dataout = open(args.output, "w")
     for index, row in enumerate(msd.T):
         print('Element:', index + 1)
         ax.plot(np.array(list(range(len(row)))) * timestep, row, label='Element: ' + str(index + 1))
-        D = CalcMSD(row)
-        dataout.write(str(D)+'\n')
+        D, D_error = CalcMSD(row)
+        dataout.write(str(D) + '\n')
+        errorout.write(str(D_error) + '\n')
     ax.legend()
+
+dataout.close()
+errorout.close()
 
 ax.set_xlabel('Time (fs)')
 ax.set_ylabel('MSD (A^2)')
