bf6ef905e6d3e7ec2c400b135c84317a4d2f18be
Yihang Peng <ypeng0082@gmail.com>
2025-08-23 22:15:39 -0400

    update

--- occ_solver.py
+++ occ_solver.py
@@ -7,6 +7,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Calculate occupancies using Fermi-Dirac distribution")
 parser.add_argument("--input", "-i", default="eqp1.dat", help="Input filename")
+parser.add_argument("--output", "-o", default="eig.dat", help="Output filename")
 parser.add_argument("--fermi_level", "-f", type=float, required=True, help="Fermi level (in eV)")
 parser.add_argument("--fermi_level_down", "-fd", type=float, default=None, help="Fermi level of spin down component (in eV)")
 parser.add_argument("--temperature", "-t", type=float, default=4000.0, help="Temperature")
@@ -15,7 +16,7 @@ parser.add_argument("-qe", action="store_true", help="Output QE-style eigenvalue
 parser.add_argument("-vasp", action="store_true", help="Output VASP-style eigenvalues and occupancies")
 
 args = parser.parse_args()
-0
+
 input_filename = args.input
 E_F_up = args.fermi_level
 
@@ -60,7 +61,7 @@ M = N_up - N_dn
 
 # ---- write output data ----
 output_data = np.column_stack((band_index, e_up, e_dn, occ_up, occ_dn))
-np.savetxt("eig.dat", output_data, fmt=['%d', '%.10f', '%.10f', '%.10f', '%.10f'], header=f"{N_total} 1 {len(band_index)}\n\n0.0 0.0 0.0 1.0", comments='')
+np.savetxt(args.output, output_data, fmt=['%d', '%.10f', '%.10f', '%.10f', '%.10f'], header=f"{N_total} 1 {len(band_index)}\n\n0.0 0.0 0.0 1.0", comments='')
 
 if args.qe:
     with open("et_in.dat", "w") as f:
